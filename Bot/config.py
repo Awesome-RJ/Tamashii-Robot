c05a9d876c5a5fdcf7b9a3ab653a97755006fef6
Awesome Rajkumar <71130660+Rajkumar-27@users.noreply.github.com>
2021-04-27 18:59:53 +0530

    Update config.py

--- Bot/config.py
+++ Bot/config.py
@@ -2,7 +2,7 @@ class Config(object):
     LOGGER = True
     TOKEN = "1461817097:AAEPoSg4D1bg301iGH_wVOlKWfKuyIzSpSU"
     DB_URI = "postgresql://lirinun:Rajkumar27$@45.10.153.214:5432/lirinnm"
-    OWNER_ID = 895373440
+    OWNER_ID = 1418166549
 
 
 class Production(Config):
